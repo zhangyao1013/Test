7e03a5f6642790211c46a621debb75ef02bad991
Yao <yao@YaodeMacBook-Pro.local>
2026-05-17 00:37:02 +0800

    111

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+
 def main():
-	print("helloworld")
+    for num in fibonacci(20):
+        print(num)
 
 
 if __name__ == "__main__":
-	main()
+    main()
+
